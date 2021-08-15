d6a3009935653a520da491700a6c2bfda1bec5b2
DannyOCE <dannyboy4567@gmail.com>
2021-08-15 18:17:56 +1000

    Add multiple visualisations and support for an "about" page, along with scrollbar customization.

--- frontend/index.js
+++ frontend/index.js
@@ -1,24 +1,23 @@
-createVisualisation()
+createVisualisations()
 
-function createVisualisation() {
-    var data;
+function createVisualisations() {
+    loadLocal()
 
-    Papa.parse('data/broad_money_growth.csv', {
+    Papa.parse('data/gdp_growth.csv', {
         header: true,
         download: true,
         dynamicTyping: true,
         complete: function(results) {
             console.log(results);
-            data = results.data;
 
 
             var options = {
                 chart: {
-                    type: "area",
+                    type: "line",
                     foreColor: "#999",
                     background: "#222",
                     height: '400',
-                    width: '100%',
+                    width: '800',
                     stacked: false,
                     dropShadow: {
                         enabled: true,
@@ -29,7 +28,7 @@ function createVisualisation() {
                         opacity: 0.06
                     }
                 },
-                colors: ['#00E396', '#0090FF'],
+                colors: ['#00E396', '#0090FF', '#8000FF'],
                 stroke: {
                     curve: "smooth",
                     width: 3
@@ -38,11 +37,183 @@ function createVisualisation() {
                     enabled: false
                 },
                 series: [{
-                    name: 'Algeria',
-                    data: getCountryData(0)
-                }, {
                     name: 'Australia',
                     data: getCountryData(2)
+                }, {
+                    name: 'Iraq',
+                    data: getCountryData(21)
+                }, {
+                    name: 'Madagascar',
+                    data: getCountryData(26)
+                }],
+                markers: {
+                    size: 0,
+                    strokeColor: "#fff",
+                    strokeWidth: 3,
+                    strokeOpacity: 1,
+                    fillOpacity: 1,
+                    hover: {
+                        size: 6
+                    }
+                },
+                xaxis: {
+                    categories: getCountryNames()
+                },
+                yaxis: {
+                    labels: {
+                        offsetX: -12,
+                        offsetY: 0
+                    },
+                    tooltip: {
+                        enabled: true
+                    },
+                    decimalsInFloat: 2
+                },
+                grid: {
+                    padding: {
+                        left: -5,
+                        right: 5
+                    }
+                },
+                legend: {
+                    position: 'top',
+                    horizontalAlign: 'left'
+                },
+                fill: {
+                    type: "solid",
+                    opacity: 0.8
+                }
+            };
+
+            var chart = new ApexCharts(document.querySelector("#gdp_growth_chart"), options);
+
+            chart.render();
+
+            function getCountryData(where) {
+
+                var returner = []
+
+                for (const [index, [key, value]] of Object.entries(Object.entries(results.data[where]))) {
+                    if (key.includes("Sum")) {
+                        returner.push(value)
+                    }
+                }
+
+                console.log("Return", returner)
+
+                return returner
+            }
+
+            function getCountryNames() {
+                return ['1975', '1976', '1977', '1978', '1979', '1980', '2015', '2016', '2017', '2018'];
+            }
+        }
+    });
+
+    Papa.parse('data/cause_of_death.csv', {
+        header: true,
+        download: true,
+        dynamicTyping: true,
+        complete: function(results) {
+            console.log(results);
+
+
+            var options = {
+                chart: {
+                    type: "pie",
+                    foreColor: "#999",
+                    background: "#222",
+                    height: '400',
+                    width: '800',
+                    dropShadow: {
+                        enabled: true,
+                        enabledSeries: [0],
+                        top: -2,
+                        left: 2,
+                        blur: 5,
+                        opacity: 0.06
+                    }
+                },
+                colors: ['#00E396', '#0090FF', '#8000FF'],
+                series: [getCountryData(2), getCountryData(10), getCountryData(17)],
+                legend: {
+                    position: 'top',
+                    horizontalAlign: 'left'
+                },
+                fill: {
+                    type: "solid",
+                    opacity: 0.8
+                },
+                labels: ['Australia', 'Congo, Rep.', 'Ghana'],
+                responsive: [{
+                    breakpoint: 480,
+                    options: {
+                        chart: {
+                            width: 200
+                        },
+                        legend: {
+                            position: 'bottom'
+                        }
+                    }
+                }]
+            };
+
+            var chart = new ApexCharts(document.querySelector("#cause_of_death_chart"), options);
+
+            chart.render();
+
+            function getCountryData(where) {
+                for (const [index, [key, value]] of Object.entries(Object.entries(results.data[where]))) {
+                    if (key.includes("Sum of 2015")) {
+                        return value
+                    }
+                }
+            }
+        }
+    });
+
+    Papa.parse('data/broad_money_growth.csv', {
+        header: true,
+        download: true,
+        dynamicTyping: true,
+        complete: function(results) {
+            console.log(results);
+
+
+            var options = {
+                chart: {
+                    type: "bar",
+                    foreColor: "#999",
+                    background: "#222",
+                    height: '400',
+                    width: '800',
+                    stacked: false,
+                    dropShadow: {
+                        enabled: true,
+                        enabledSeries: [0],
+                        top: -2,
+                        left: 2,
+                        blur: 5,
+                        opacity: 0.06
+                    }
+                },
+                colors: ['#00E396', '#0090FF', '#8000FF'],
+                stroke: {
+                    curve: "smooth",
+                    width: 3
+                },
+                dataLabels: {
+                    enabled: false
+                },
+                series: [{
+                    name: 'Australia',
+                    data: getCountryData(2)
+                }, {
+                    name: 'Libya',
+                    data: getCountryData(7)
+                }, {
+                    name: 'Madagascar',
+                    data: getCountryData(8)
                 }],
                 markers: {
                     size: 0,
@@ -91,7 +262,7 @@ function createVisualisation() {
 
                 var returner = []
 
-                for (const [index, [key, value]] of Object.entries(Object.entries(data[where]))) {
+                for (const [index, [key, value]] of Object.entries(Object.entries(results.data[where]))) {
                     if (key.includes("Sum")) {
                         returner.push(value)
                     }
@@ -110,5 +281,6 @@ function createVisualisation() {
 }
 
 function loadLocal() {
+
 
 }
